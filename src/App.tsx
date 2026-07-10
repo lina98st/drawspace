708b9da21b4487dbdefbbde35eb32e6951fc1e4f
lina98st <156576799+lina98st@users.noreply.github.com>
2026-07-10 16:58:16 +0200

    Add canvas element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { useRef } from "react";
+
 function App() {
-  return <div></div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={800}
+      height={600}
+      style={{ border: "1px solid #ccc" }}
+    />
+  );
 }
 
 export default App;
